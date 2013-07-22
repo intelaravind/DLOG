34e817185a49ce4389c4c1437452400a8461d002
Aravind <aravindsr.india@gmail.com>
2013-07-22 11:08:37 +0200

    Enabled wrapper

--- DLOG.hpp
+++ DLOG.hpp
@@ -26,46 +26,46 @@ std::map<std::string, aravdebug*> path_to_debug_map;
 /***
  * Wrapper for aravdebug
  */
-class MDEBG
+class DLOG
 {
 	aravdebug *dbg;
 	int should_free = 1;
 
 public:
-	MDEBG(const char * userfile, int lineno, const char *PATH)
+	DLOG(const char * userfile, int lineno, const char *PATH)
 	{
-//		if (path_to_debug_map.find(PATH) != path_to_debug_map.end())
-//		{
-//			dbg = path_to_debug_map.find(PATH)->second;
-//			should_free = 0;
-//			/*
-//			 if (filemode == FILE_NEW)
-//			 {
-//			 std::cout
-//			 << "WARNING (MDEBG): OBJECT WITH SAME PATH WAS CREATED EARLIER.\n"
-//			 << "\tIGNORING THE REQUEST TO CREATE A NEW FILE ::" << userfile<<":"<<lineno;
-//			 }
-//			 */
-//		}
-//		else
-//		{
-//			dbg = new aravdebug(userfile, lineno, PATH);
-//
-//			path_to_debug_map[PATH] = dbg;
-//		}
+		if (path_to_debug_map.find(PATH) != path_to_debug_map.end())
+		{
+			dbg = path_to_debug_map.find(PATH)->second;
+			should_free = 0;
+			/*
+			 if (filemode == FILE_NEW)
+			 {
+			 std::cout
+			 << "WARNING (MDEBG): OBJECT WITH SAME PATH WAS CREATED EARLIER.\n"
+			 << "\tIGNORING THE REQUEST TO CREATE A NEW FILE ::" << userfile<<":"<<lineno;
+			 }
+			 */
+		}
+		else
+		{
+			dbg = new aravdebug(userfile, lineno, PATH);
+
+			path_to_debug_map[PATH] = dbg;
+		}
 	}
 
 	template<typename T>
 	void print_to_file(const char * userfile, int lineno, T obj)
 	{
-		//dbg->print_to_file(userfile, lineno, obj);
+		dbg->print_to_file(userfile, lineno, obj);
 	}
 
 	template<typename T>
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			T* obj)
 	{
-		//dbg->print_to_file(userfile, lineno, tag, obj);
+		dbg->print_to_file(userfile, lineno, tag, obj);
 	}
 
 //	template<typename T>
@@ -79,48 +79,47 @@ public:
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			T &obj)
 	{
-		//this->print_to_file(userfile, lineno, tag, &obj);
+		this->print_to_file(userfile, lineno, tag, &obj);
 	}
 
 	template<typename T>
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			T* obj, ADDON addon)
 	{
-		//dbg->print_to_file(userfile, lineno, tag, obj, addon);
+		dbg->print_to_file(userfile, lineno, tag, obj, addon);
 	}
 
 	template<typename T>
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			T obj, ADDON addon)
 	{
-		//dbg->print_to_file(userfile, lineno, &tag, obj, addon);
+		dbg->print_to_file(userfile, lineno, &tag, obj, addon);
 	}
 
 	template<typename T>
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			T &obj, ADDON addon)
 	{
-		//dbg->print_to_file(userfile, lineno, &tag, obj, addon);
+		dbg->print_to_file(userfile, lineno, &tag, obj, addon);
 	}
 
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			unsigned int obj)
 	{
-		//dbg->print_to_file(userfile, lineno, tag, &obj);
+		dbg->print_to_file(userfile, lineno, tag, &obj);
 	}
 
 	void print_to_file(const char * userfile, int lineno, const char* tag,
 			int obj)
 	{
-		//dbg->print_to_file(userfile, lineno, tag, &obj);
+		dbg->print_to_file(userfile, lineno, tag, &obj);
 	}
 
-	~MDEBG()
+	~DLOG()
 	{
 		errs() << "Calling ~MDEBG\n";
-//
-//		if (should_free)
-//			delete dbg;
+		if (should_free)
+			delete dbg;
 	}
 
 };
