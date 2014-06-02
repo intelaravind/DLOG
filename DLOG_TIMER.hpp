08eb20004c7d219ff6e9c0d81e87cc45215a95aa
Aravind <aravind.sukumaran-rajam@inria.fr>
2014-06-02 13:15:42 +0200

    timer simplified

--- DLOG_TIMER.hpp
+++ DLOG_TIMER.hpp
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sstream>
+#include <algorithm>
 
 template<class T>
 static std::string dlog_to_string(T input)
@@ -25,116 +26,178 @@ static std::string dlog_to_string(T input)
 static std::string humanreadabletime(long input)
 {
 
-	input = input/1000000; // max accuray we need is milli second
+	input = input / 1000000; // max accuray we need is milli second
 
-	std::string hour = dlog_to_string(input / ((long)1000 * 60 * 60));
+	std::string hour = dlog_to_string(input / ((long) 1000 * 60 * 60));
 
 	input = input % (1000 * 60 * 60);
-	std::string min = dlog_to_string(input / ((long)1000 * 60));
+	std::string min = dlog_to_string(input / ((long) 1000 * 60));
 
 	input = input % (1000 * 60);
-	std::string sec = dlog_to_string(input / ((double)1000));
+	std::string sec = dlog_to_string(input / ((double) 1000));
 
 	std::string ret = hour + "h : " + min + "m : " + sec + " s";
 	return ret;
 
 }
+
+class DLOG_TIMER_OPTIONS
+{
+public:
+	std::string delimiter = "\t";
+	bool human_readable = true;
+};
+
 class DLOG_TIMER
 {
 	std::vector<struct timespec> start_list;
-	std::vector<long> times;
-	std::vector<std::string> names;
+	std::vector<long> cummulative_time;
+	std::vector<std::string> timer_names;
+
+	int get_id_by_name(std::string name)
+	{
+		auto it = std::find(timer_names.begin(), timer_names.end(), name);
+		int id = it - timer_names.begin();
+		return id;
+	}
 
 public:
 	DLOG_TIMER()
 	{
 
 	}
-	int start_new(std::string name = "no name")
+	void start(std::string name)
 	{
 		struct timespec start;
 		if (clock_gettime(CLOCK_REALTIME, &start) == -1)
 		{
 			perror("clock gettime");
-			exit (EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
 		start_list.push_back(start);
-		times.push_back(0);
-		names.push_back(name);
-		return times.size() - 1;
+		cummulative_time.push_back(0);
+		timer_names.push_back(name);
 	}
-	long int stop(int id)
+
+	long int stop(std::string name)
 	{
 		struct timespec stop;
 		if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
 		{
 			perror("clock gettime");
-			exit (EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
+
+		int id = get_id_by_name(name);
 
 		auto val = 1000000000 * (stop.tv_sec - start_list[id].tv_sec)
 				+ (stop.tv_nsec - start_list[id].tv_nsec);
 
-		times[id] += val;
-		return times[id];
+		cummulative_time[id] += val;
+		return cummulative_time[id];
 	}
 
-	long int pause(int id)
+	long int pause(std::string name)
 	{
-		return stop(id);
+		return stop(name);
 	}
 
-	void resume(int id)
+	void resume(std::string name)
 	{
+		int id = get_id_by_name(name);
 //		std::cout<<"Id = "<<id<<" times "<< times[id] <<"\n";
 		struct timespec start;
 		if (clock_gettime(CLOCK_REALTIME, &start) == -1)
 		{
 			perror("clock gettime");
-			exit (EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
 		start_list[id] = start;
 	}
 
-	void reset(int id)
+	void reset(std::string name)
 	{
+		int id = get_id_by_name(name);
 		struct timespec start;
 		if (clock_gettime(CLOCK_REALTIME, &start) == -1)
 		{
 			perror("clock gettime");
-			exit (EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
 		start_list[id] = start;
 	}
 
-	void draw_table(std::string filename, std::string tablename,
-			std::string path = "")
+	void output_html_table(std::string filename, std::string tablename =
+			"unnamed", std::string path = "", DLOG_TIMER_OPTIONS options =
+			DLOG_TIMER_OPTIONS())
 	{
 
 		if (path == "")
 			path = getenv("DLOG_OUTPUT_FOLDER");
 
 		DLOG_TABLE table(filename.c_str(), tablename.c_str(), path);
-		std::vector < std::string > head_row;
-		head_row.push_back("Id");
+		std::vector<std::string> head_row;
+
 		head_row.push_back("Name");
 		head_row.push_back("Time");
+
+		if (options.human_readable)
+		{
+			head_row.push_back("Human readable");
+		}
 		table.insert_row(head_row);
 
-		for (unsigned int i = 0; i < times.size(); i++)
+		for (unsigned int i = 0; i < cummulative_time.size(); i++)
 		{
-			std::vector < std::string > row;
+			std::vector<std::string> row;
 			std::string str_i = dlog_to_string(i);
-			std::string str_times = dlog_to_string(times[i]);
+			std::string str_times = dlog_to_string(cummulative_time[i]);
 
-			row.push_back(str_i);
-			row.push_back(names[i]);
-			row.push_back(str_times + "  (" + humanreadabletime(times[i])+ " )");
+			row.push_back(timer_names[i]);
+			row.push_back(str_times);
+			if (options.human_readable)
+			{
+			row.push_back(
+					"  (" + humanreadabletime(cummulative_time[i]) + " )");
+			}
 
 			table.insert_row(row);
 
 		}
 		table.table_html_dump();
+	}
+
+	void output_stream(FILE *fp, std::string tablename = "unnamed",
+			 DLOG_TIMER_OPTIONS options =
+					DLOG_TIMER_OPTIONS())
+	{
+
+		fprintf(fp,"%s\n",tablename.c_str());
+		fprintf(fp, "Name%sTime%sHuman_readable\n", options.delimiter.c_str(),options.delimiter.c_str());
+		fprintf(fp,"=============================================================================\n");
+
+		for (unsigned int i = 0; i < cummulative_time.size(); i++)
+		{
+			std::vector<std::string> row;
+			std::string str_i = dlog_to_string(i);
+			std::string str_times = dlog_to_string(cummulative_time[i]);
+
+			if (options.human_readable == true)
+			{
+				std::string hr = "  (" + humanreadabletime(cummulative_time[i])
+								+ " )";
+				fprintf(fp, "%s%s%s%s%s\n", (timer_names[i]).c_str(), options.delimiter.c_str(),str_times.c_str(), options.delimiter.c_str(),hr.c_str());
+
+			}
+			else
+			{
+			fprintf(fp, "%s%s%s\n", (timer_names[i]).c_str(), options.delimiter.c_str(),
+								str_times.c_str());
+			}
+
+
+
+		}
 	}
 };
 
