b6cc34b477a840d8f2a5febb58f23b7775a87da6
Aravind Sukumaran Rajam <aravind.sukumaran-rajam@inria.fr>
2015-02-13 15:00:14 +0100

    gantt chart

--- DLOG_TABLE.hpp
+++ DLOG_TABLE.hpp
@@ -16,308 +16,348 @@ typedef std::vector<std::string> t_row;
 class DLOG_TABLE
 {
 
-	std::string dataPath;
-	std::string outputfilename;
+    std::string dataPath;
+    std::string outputfilename;
 
 public:
-	std::vector<std::string> table_head_row;
-	std::vector<std::string> table_head_column;
-	std::vector<t_row> values;
-	std::string table_name;
-	int cur_row = 0;
-	int drawGraph = 0;
+    std::vector<std::string> table_head_row;
+    std::vector<std::string> table_head_column;
+    std::vector<t_row> values;
+    std::string table_name;
+    int cur_row = 0;
+    int drawGraph = 0;
 
+    DLOG_TABLE(const char *OUT_FILE, const char *tbl_name, std::string inpPath = getenv("DLOG_OUTPUT_FOLDER"))
+    {
+        dataPath = inpPath;
+        table_name = tbl_name;
+        outputfilename = OUT_FILE;
+    }
 
-	DLOG_TABLE(const char *OUT_FILE, const char *tbl_name, std::string inpPath =
-			getenv("DLOG_OUTPUT_FOLDER"))
-	{
-		dataPath = inpPath;
-		table_name = tbl_name;
-		outputfilename = OUT_FILE;
-	}
+    void insert_row(std::vector<std::string> inp)
+    {
+        t_row temp_values;
 
-	void insert_row(std::vector<std::string> inp)
-	{
-		t_row temp_values;
+        for (auto iter : inp)
+        {
+            temp_values.push_back(iter);
+        }
+        values.push_back(temp_values);
+    }
 
-		for (auto iter : inp)
-		{
-			temp_values.push_back(iter);
-		}
-		values.push_back(temp_values);
-	}
+    void insert_head_row(std::vector<std::string> &inp)
+    {
+        t_row temp_values;
+        for (auto iter : inp)
+        {
+            temp_values.push_back(iter);
+        }
+        values.insert(values.begin(), temp_values);
+    }
 
-	void insert_head_row(std::vector<std::string> &inp)
-	{
-		t_row temp_values;
-		for (auto iter : inp)
-		{
-			temp_values.push_back(iter);
-		}
-		values.insert(values.begin(), temp_values);
-	}
+    void table_emit_graph_div(std::fstream &fwrite)
+    {
+        fwrite << "<div id=\"container" << filter_string(table_name) << "\" style=\"max-width: 1024px; height: 400px; margin: 0 auto\"></div>\n";
+    }
 
-	void table_emit_graph_div(std::fstream &fwrite)
-	{
-		fwrite << "<div id=\"container" << filter_string(table_name)
-				<< "\" style=\"max-width: 1024px; height: 400px; margin: 0 auto\"></div>\n";
-	}
+    void table_emit_graph_javascript(std::fstream &fwrite)
+    {
+        fwrite << "<script type=\"text/javascript\">\n"
+               "(function($){\n"
+               "$(function () {\n"
+               "$('#" << "container" << filter_string(table_name) << "').highcharts({\n"
+               "chart: {\n"
+               "type: 'column',\n"
+               "zoomType: 'xy'\n"
+               "},\n"
+               "title: {"
+               "text: '" << table_name << "'\n"
+               " },\n"
+               "xAxis: {\n"
+               "categories: [\n";
 
-	void table_emit_graph_javascript(std::fstream &fwrite)
-	{
-		fwrite << "<script type=\"text/javascript\">\n"
-				"(function($){\n"
-				"$(function () {\n"
-				"$('#" << "container" << filter_string(table_name)
-				<< "').highcharts({\n"
-						"chart: {\n"
-						"type: 'column',\n"
-						"zoomType: 'xy'\n"
-						"},\n"
-						"title: {"
-						"text: '" << table_name << "'\n"
-						" },\n"
-						"xAxis: {\n"
-						"categories: [\n";
+        //The first column is item (this is not a category. So skip it.
+        int isfirst = 1;
+        for (auto temphead : values.at(0))
+        {
+            if (isfirst == 1)
+                isfirst = 0;
+            else
+                fwrite << "'" << temphead << "',\n";
+        }
+        fwrite << "]\n"
+               "},\n"
+               "yAxis:\n"
+               "{\n"
+               "min: 0,\n"
+               "title:\n"
+               "{\n"
+               "text: 'params'\n"
+               "}\n"
+               "},\n"
+               "tooltip: {\n"
+               "},\n"
+               "plotOptions:\n"
+               "{\n"
+               "	column:\n"
+               "	{\n"
+               "		pointPadding: 0.2,\n"
+               "		borderWidth: 0\n"
+               "	}\n"
+               "},\n"
+               "series: [\n";
 
-		//The first column is item (this is not a category. So skip it.
-		int isfirst = 1;
-		for (auto temphead : values.at(0))
-		{
-			if (isfirst == 1)
-				isfirst = 0;
-			else
-				fwrite << "'" << temphead << "',\n";
-		}
-		fwrite << "]\n"
-				"},\n"
-				"yAxis:\n"
-				"{\n"
-				"min: 0,\n"
-				"title:\n"
-				"{\n"
-				"text: 'params'\n"
-				"}\n"
-				"},\n"
-				"tooltip: {\n"
-				"},\n"
-				"plotOptions:\n"
-				"{\n"
-				"	column:\n"
-				"	{\n"
-				"		pointPadding: 0.2,\n"
-				"		borderWidth: 0\n"
-				"	}\n"
-				"},\n"
-				"series: [\n";
+        int rowindex = 0;
+        for (auto temprow : values)
+        {
+            //skip first row because its the header row
+            if (temprow == values.at(0))
+                continue;
+            fwrite << "{\n";
+            int isfirst = 1;
+            for (auto temval : temprow)
+            {
+                if (isfirst == 1)
+                {
+                    fwrite << "name: '" << temval << "', data\n: [";
+                    isfirst = 0;
+                    rowindex++;
+                }
+                else
+                    fwrite << temval << ",";
+            }
 
-		int rowindex = 0;
-		for (auto temprow : values)
-		{
-			//skip first row because its the header row
-			if (temprow == values.at(0))
-				continue;
-			fwrite << "{\n";
-			int isfirst = 1;
-			for (auto temval : temprow)
-			{
-				if (isfirst == 1)
-				{
-					fwrite << "name: '" << temval << "', data\n: [";
-					isfirst = 0;
-					rowindex++;
-				}
-				else
-					fwrite << temval << ",";
-			}
+            fwrite << "]},\n";
+        }
 
-			fwrite << "]},\n";
-		}
+        fwrite << "]});\n"
+               "});\n"
+               "})(jQuery);\n"
+               "</script>";
+    }
 
-		fwrite << "]});\n"
-				"});\n"
-				"})(jQuery);\n"
-				"</script>";
-	}
+    void table_html_dump_unwrap(std::fstream &fwrite, int hold)
+    {
 
-	void table_html_dump_unwrap(std::fstream &fwrite, int hold)
-	{
+        fwrite << "<h1>Table: " << table_name << "</h1>" << "\n";
 
-		fwrite << "<h1>Table: " << table_name << "</h1>" << "\n";
+        unsigned int n_cols = (values.at(0)).size();
+        unsigned int n_rows = values.size();
+        unsigned int i, j;
 
-		unsigned int n_cols = (values.at(0)).size();
-		unsigned int n_rows = values.size();
-		unsigned int i, j;
+        if (hold == 0)
+        {
+            table_html_header(fwrite);
+            if (drawGraph == 1)
+            {
+                table_emit_graph_javascript(fwrite);
+            }
+            table_html_header_end(fwrite);
+        }
 
-		if (hold == 0)
-		{
-			table_html_header(fwrite);
-			if (drawGraph == 1)
-			{
-				table_emit_graph_javascript(fwrite);
-			}
-			table_html_header_end(fwrite);
-		}
+        fwrite << "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" id=\"" << filter_string(table_name) << "\" class=\"sortable\">\n";
 
-		fwrite
-				<< "<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" id=\""
-				<< filter_string(table_name) << "\" class=\"sortable\">\n";
+        fwrite << "<thead><tr>" << "\n";
+        for (i = 0; i < n_cols; i++)
+        {
+            fwrite << "<th><h3>";
+            fwrite << (values.at(0)).at(i);
+            fwrite << "</h3></th>\n";
+        }
+        fwrite << "</tr></thead>" << "\n";
 
-		fwrite << "<thead><tr>" << "\n";
-		for (i = 0; i < n_cols; i++)
-		{
-			fwrite << "<th><h3>";
-			fwrite << (values.at(0)).at(i);
-			fwrite << "</h3></th>\n";
-		}
-		fwrite << "</tr></thead>" << "\n";
+        for (i = 1; i < n_rows; i++)
+        {
+            fwrite << "<tr>" << "\n";
+            t_row *row = &values.at(i);
+            for (j = 0; j < row->size(); j++)
+            {
+                fwrite << "<td>";
+                fwrite << row->at(j);
+                fwrite << "</td>" "\n";
+            }
+            fwrite << "</tr>" << "\n";
+        }
+        fwrite << "</table>\n";
 
-		for (i = 1; i < n_rows; i++)
-		{
-			fwrite << "<tr>" << "\n";
-			t_row *row = &values.at(i);
-			for (j = 0; j < row->size(); j++)
-			{
-				fwrite << "<td>";
-				fwrite << row->at(j);
-				fwrite << "</td>" "\n";
-			}
-			fwrite << "</tr>" << "\n";
-		}
-		fwrite << "</table>\n";
+        if (hold == 0)
+        {
+            if (drawGraph == 1)
+            {
+                table_emit_graph_div(fwrite);
+            }
+            std::vector < std::string > tempvec;
+            tempvec.push_back(table_name);
+            table_html_footer(fwrite, tempvec);
 
-		if (hold == 0)
-		{
-			if (drawGraph == 1)
-			{
-				table_emit_graph_div(fwrite);
-			}
-			std::vector < std::string > tempvec;
-			tempvec.push_back(table_name);
-			table_html_footer(fwrite, tempvec);
+            fwrite.close();
+        }
 
-			fwrite.close();
-		}
+        std::string syscommand;
+        //copy the recovery script
+        unsigned found = dataPath.find_last_of("/\\");
+        syscommand = "cp -r $DLOG_PATH/tinytable " + dataPath.substr(0, found);
+        int status = system(syscommand.c_str());
+        if (status < 0)
+            std::cout << "DLOG Table Error: " << strerror(errno) << '\n';
+    }
 
-		std::string syscommand;
-		//copy the recovery script
-		unsigned found = dataPath.find_last_of("/\\");
-		syscommand = "cp -r $DLOG_PATH/tinytable " + dataPath.substr(0, found);
-		int status = system(syscommand.c_str());
-		if (status < 0)
-			std::cout << "DLOG Table Error: " << strerror(errno) << '\n';
-	}
+    void table_html_dump()
+    {
+        std::fstream fwrite;
 
-	void table_html_dump()
-	{
-		std::fstream fwrite;
+        std::string fullpath = dataPath + outputfilename;
+        //std::cerr<<"************fullpath = "<<fullpath<<"\n";
+        fwrite.open(fullpath.c_str(), std::fstream::out);
+        table_html_dump_unwrap(fwrite, NOHOLD);
+    }
 
-		std::string fullpath = dataPath + outputfilename;
-		//std::cerr<<"************fullpath = "<<fullpath<<"\n";
-		fwrite.open(fullpath.c_str(), std::fstream::out);
-		table_html_dump_unwrap(fwrite, NOHOLD);
-	}
+    void table_gantt_dump()
+    {
+        std::fstream fwrite;
+        std::string fullpath = dataPath + outputfilename;
+        fwrite.open(fullpath.c_str(), std::fstream::out);
 
-	void table_dump()
-	{
-		std::cout << "Table: " << table_name << "\n";
+        fwrite << R"(
+    <html>
+    <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["timeline"]});
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var container = document.getElementById('timeline');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();)";
+
+        fwrite << "\n" << R"(
+        dataTable.addColumn({ type: 'string', id: 'Event' });
+        dataTable.addColumn({ type: 'number', id: 'Start' });
+        dataTable.addColumn({ type: 'number', id: 'End' });)" << "\n";
+
+        unsigned int n_rows = values.size();
+
+        fwrite << "dataTable.addRows([";
+        for (unsigned int i = 0; i < n_rows; i++)
+        {
+            t_row *row = &values.at(i);
+            fwrite << "[ '" << row->at(0) << "', " << row->at(1) << ", " << row->at(2) << "],\n";
+        }
+        fwrite << "]);\n";
+
+        fwrite << R"(chart.draw(dataTable);
+      }
+    </script>
+    </head>
+      <body>
+        <div id="timeline" style="width: 900px; height: 180px;"></div>
+      </body>
+    </html>)";
+
+        fwrite.close();
+
+    }
+
+    void table_dump()
+    {
+        std::cout << "Table: " << table_name << "\n";
 //	unsigned int n_cols = (values.at(0)).size();
-		unsigned int n_rows = values.size();
-		unsigned int i, j;
+        unsigned int n_rows = values.size();
+        unsigned int i, j;
 
-		for (i = 0; i < n_rows; i++)
-		{
-			t_row *row = &values.at(i);
-			for (j = 0; j < row->size(); j++)
-			{
-				std::cout << row->at(j) << "\t\t";
-			}
-			std::cout << "\n";
-		}
-		std::cout << "\n\n";
-	}
+        for (i = 0; i < n_rows; i++)
+        {
+            t_row *row = &values.at(i);
+            for (j = 0; j < row->size(); j++)
+            {
+                std::cout << row->at(j) << "\t\t";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n\n";
+    }
 };
 
 class DLOG_TABLES
 {
-	std::vector<DLOG_TABLE> tables;
-	std::vector<int> header_row_populated;
-	std::string dataPath;
+    std::vector<DLOG_TABLE> tables;
+    std::vector<int> header_row_populated;
+    std::string dataPath;
 
 public:
-	DLOG_TABLES(const char *OUT_FILE,
-			std::string inpPath = getenv("DLOG_OUTPUT_FOLDER"))
-	{
+    DLOG_TABLES(const char *OUT_FILE, std::string inpPath = getenv("DLOG_OUTPUT_FOLDER"))
+    {
 
-		dataPath = inpPath + std::string(OUT_FILE);
-		std::cout << "created tabales : " << dataPath << "\n";
-	}
+        dataPath = inpPath + std::string(OUT_FILE);
+        std::cout << "created tabales : " << dataPath << "\n";
+    }
 
-	void show_graph(TID tid)
-	{
-		tables.at(tid).drawGraph = 1;
-	}
+    void show_graph(TID tid)
+    {
+        tables.at(tid).drawGraph = 1;
+    }
 
-	TID newtable(const char *tablename)
-	{
+    TID newtable(const char *tablename)
+    {
 
-		DLOG_TABLE *temptable = new DLOG_TABLE(dataPath.c_str(), tablename);
-		tables.push_back(*temptable);
-		header_row_populated.push_back(0);
-		return TID(tables.size() - 1);
-	}
+        DLOG_TABLE *temptable = new DLOG_TABLE(dataPath.c_str(), tablename);
+        tables.push_back(*temptable);
+        header_row_populated.push_back(0);
+        return TID(tables.size() - 1);
+    }
 
-	void insert_head_row(TID tid, std::vector<std::string> &inp)
-	{
-		if (header_row_populated[tid] == 0)
-		{
-			tables.at(tid).insert_head_row(inp);
-			header_row_populated[tid] = 1;
-		}
-	}
+    void insert_head_row(TID tid, std::vector<std::string> &inp)
+    {
+        if (header_row_populated[tid] == 0)
+        {
+            tables.at(tid).insert_head_row(inp);
+            header_row_populated[tid] = 1;
+        }
+    }
 
-	void insert_row(TID tid, std::vector<std::string> &inp)
-	{
-		tables.at(tid).insert_row(inp);
-	}
+    void insert_row(TID tid, std::vector<std::string> &inp)
+    {
+        tables.at(tid).insert_row(inp);
+    }
 
-	void html_dump()
-	{
-		std::fstream fwrite;
-		fwrite.open(dataPath.c_str(), std::fstream::out);
+    void html_dump()
+    {
+        std::fstream fwrite;
+        fwrite.open(dataPath.c_str(), std::fstream::out);
 
-		table_html_header(fwrite);
-		for (auto temptable : tables)
-		{
-			if (temptable.values.size() == 0)
-				continue;
-			else if (temptable.drawGraph == 1)
-			{
-				temptable.table_emit_graph_javascript(fwrite);
-			}
-		}
+        table_html_header(fwrite);
+        for (auto temptable : tables)
+        {
+            if (temptable.values.size() == 0)
+                continue;
+            else if (temptable.drawGraph == 1)
+            {
+                temptable.table_emit_graph_javascript(fwrite);
+            }
+        }
 
-		table_html_header_end(fwrite);
-		std::vector < std::string > names;
+        table_html_header_end(fwrite);
+        std::vector < std::string > names;
 
-		for (auto temptable : tables)
-		{
-			if (temptable.values.size() == 0)
-				continue;
-			else
-			{
-				names.push_back(temptable.table_name);
-				temptable.table_html_dump_unwrap(fwrite, HOLD);
-				if (temptable.drawGraph == 1)
-				{
-					temptable.table_emit_graph_div(fwrite);
-				}
-			}
-		}
+        for (auto temptable : tables)
+        {
+            if (temptable.values.size() == 0)
+                continue;
+            else
+            {
+                names.push_back(temptable.table_name);
+                temptable.table_html_dump_unwrap(fwrite, HOLD);
+                if (temptable.drawGraph == 1)
+                {
+                    temptable.table_emit_graph_div(fwrite);
+                }
+            }
+        }
 
-		table_html_footer(fwrite, names);
+        table_html_footer(fwrite, names);
 
-	}
+    }
 
 };
 
--- DLOG_TIMER.hpp
+++ DLOG_TIMER.hpp
@@ -14,7 +14,7 @@
 #include <cstdlib>
 #include <sstream>
 #include <algorithm>
-
+#include <limits>
 #include <mutex>
 
 #include <assert.h>
@@ -27,213 +27,259 @@ struct unfinished_event;
 template<class T>
 static std::string dlog_to_string(T input)
 {
-    std::ostringstream convert;
-    convert << input;
-    return convert.str();
+	std::ostringstream convert;
+	convert << input;
+	return convert.str();
 }
 
-static long time_to_long(const struct timespec& t) {
-    return t.tv_sec * 1000000000 + t.tv_nsec;
+static long time_to_long(const struct timespec& t)
+{
+	return t.tv_sec * 1000000000 + t.tv_nsec;
+}
+
+static std::string time_to_str(const struct timespec& t)
+{
+	return std::to_string(t.tv_sec * 1000000000 + t.tv_nsec);
 }
 
 static std::string humanreadabletime(long input)
 {
 
-    input = input / 1000000; // max accuray we need is milli second
+	input = input / 1000000; // max accuray we need is milli second
 
-    std::string hour = dlog_to_string(input / ((long) 1000 * 60 * 60));
+	std::string hour = dlog_to_string(input / ((long) 1000 * 60 * 60));
 
-    input = input % (1000 * 60 * 60);
-    std::string min = dlog_to_string(input / ((long) 1000 * 60));
+	input = input % (1000 * 60 * 60);
+	std::string min = dlog_to_string(input / ((long) 1000 * 60));
 
-    input = input % (1000 * 60);
-    std::string sec = dlog_to_string(input / ((double) 1000));
+	input = input % (1000 * 60);
+	std::string sec = dlog_to_string(input / ((double) 1000));
 
-    std::string ret = hour + "h : " + min + "m : " + sec + " s";
-    return ret;
+	std::string ret = hour + "h : " + min + "m : " + sec + " s";
+	return ret;
 
 }
 
-struct event_t {
-    event_t(const char* n, struct timespec b, struct timespec e) {
-        name = n;
-        begin = b;
-        end = e;
-    }
+struct event_t
+{
+	event_t(const char* n, struct timespec b, struct timespec e)
+	{
+		name = n;
+		begin = b;
+		end = e;
+	}
 
-    struct timespec begin, end;
-    const char* name;
+	struct timespec begin, end;
+	const char* name;
 };
 
 class DLOG_TIMER_OPTIONS
 {
 public:
-    std::string delimiter = "\t";
-    bool human_readable = true;
+	std::string delimiter = "\t";
+	bool human_readable = true;
 };
 
 class DLOG_TIMER
 {
-    std::map<std::string, struct timespec> started;
-    std::vector<event_t> events;
+	std::map<std::string, struct timespec> started;
+	std::vector<event_t> events;
 
-    std::mutex mut;
+	std::mutex mut;
 
 public:
-    DLOG_TIMER(){}
+	DLOG_TIMER()
+	{
+	}
 
-    ~DLOG_TIMER(){}
+	~DLOG_TIMER()
+	{
+	}
 
-    void start(const char *name)
-    {
-        struct timespec start;
-        if (clock_gettime(CLOCK_REALTIME, &start) == -1)
-        {
-            perror("clock gettime");
-            exit(EXIT_FAILURE);
-        }
+	void start(const char *name)
+	{
+		struct timespec start;
+		if (clock_gettime(CLOCK_REALTIME, &start) == -1)
+		{
+			perror("clock gettime");
+			exit (EXIT_FAILURE);
+		}
 
-        mut.lock();
+		mut.lock();
 
-        started[name] = start;
+		started[name] = start;
 
-        mut.unlock();
-    }
+		mut.unlock();
+	}
 
-    void stop(const char* name)
-    {
-        struct timespec stop;
-        if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
-        {
-            perror("clock gettime");
-            exit(EXIT_FAILURE);
-        }
+	void stop(const char* name)
+	{
+		struct timespec stop;
+		if (clock_gettime(CLOCK_REALTIME, &stop) == -1)
+		{
+			perror("clock gettime");
+			exit (EXIT_FAILURE);
+		}
 
-        mut.lock();
+		mut.lock();
 
-        std::map<std::string, struct timespec>::iterator begin = started.find(name);
-        event_t event(name, begin->second, stop);
-        events.push_back(event);
-        started.erase(begin);
+		std::map<std::string, struct timespec>::iterator begin = started.find(name);
+		event_t event(name, begin->second, stop);
+		events.push_back(event);
+		started.erase(begin);
 
-        mut.unlock();
-    }
+		mut.unlock();
+	}
 
-    void reset() {
-        mut.lock();
+	void reset()
+	{
+		mut.lock();
 
-        started.clear();
-        events.clear();
+		started.clear();
+		events.clear();
 
-        mut.unlock();
-    }
+		mut.unlock();
+	}
 
-    std::map<std::string, long> get_cummulative_times() {
-        std::map<std::string, long> cummul;
+	std::map<std::string, long> get_cummulative_times()
+	{
+		std::map<std::string, long> cummul;
 
-        for(event_t &event : events)
-            cummul[event.name] = 0;
+		for (event_t &event : events)
+			cummul[event.name] = 0;
 
-        for(event_t &event : events)
-            cummul[event.name] += time_to_long(event.end) - time_to_long(event.begin);
+		for (event_t &event : events)
+			cummul[event.name] += time_to_long(event.end) - time_to_long(event.begin);
 
-        return cummul;
-    }
+		return cummul;
+	}
 
-    void output_html_table(std::string filename, std::string tablename =
-            "unnamed", std::string path = "", DLOG_TIMER_OPTIONS options =
-            DLOG_TIMER_OPTIONS())
-    {
+	void output_html_table(std::string filename, std::string tablename = "unnamed", std::string path = "", DLOG_TIMER_OPTIONS options = DLOG_TIMER_OPTIONS())
+	{
 
-        if (path == "")
-            path = getenv("DLOG_OUTPUT_FOLDER");
+		if (path == "")
+			path = getenv("DLOG_OUTPUT_FOLDER");
 
-        DLOG_TABLE table(filename.c_str(), tablename.c_str(), path);
-        std::vector<std::string> head_row;
+		DLOG_TABLE table(filename.c_str(), tablename.c_str(), path);
+		std::vector < std::string > head_row;
 
-        head_row.push_back("Name");
-        head_row.push_back("Time");
+		head_row.push_back("Name");
+		head_row.push_back("Time");
 
-        if (options.human_readable)
-        {
-            head_row.push_back("Human readable");
-        }
-        table.insert_row(head_row);
+		if (options.human_readable)
+		{
+			head_row.push_back("Human readable");
+		}
+		table.insert_row(head_row);
 
-        std::map<std::string, long> cummulative_time = get_cummulative_times();
+		std::map<std::string, long> cummulative_time = get_cummulative_times();
 
-        for (auto pair : cummulative_time)
-        {
-            std::vector<std::string> row;
-            row.push_back(pair.first);
-            row.push_back(dlog_to_string(pair.second));
+		for (auto pair : cummulative_time)
+		{
+			std::vector < std::string > row;
+			row.push_back(pair.first);
+			row.push_back(dlog_to_string(pair.second));
 
-            if (options.human_readable)
-            {
-                row.push_back(
-                        "  (" + humanreadabletime(pair.second) + " )");
-            }
+			if (options.human_readable)
+			{
+				row.push_back("  (" + humanreadabletime(pair.second) + " )");
+			}
 
-            table.insert_row(row);
-        }
-        table.table_html_dump();
-    }
+			table.insert_row(row);
+		}
+		table.table_html_dump();
+	}
 
-    void output_stream(FILE *fp, std::string tablename = "unnamed",
-            DLOG_TIMER_OPTIONS options = DLOG_TIMER_OPTIONS())
-    {
+	void output_stream(FILE *fp, std::string tablename = "unnamed", DLOG_TIMER_OPTIONS options = DLOG_TIMER_OPTIONS())
+	{
 
-        fprintf(fp, "%s\n", tablename.c_str());
+		fprintf(fp, "%s\n", tablename.c_str());
 
-        if (options.human_readable == true)
-        {
-            fprintf(fp, "Name%sTime%sHuman_readable\n",
-                    options.delimiter.c_str(), options.delimiter.c_str());
-        }
-        else
-        {
-            fprintf(fp, "Name%sTime\n", options.delimiter.c_str());
-        }
+		if (options.human_readable == true)
+		{
+			fprintf(fp, "Name%sTime%sHuman_readable\n", options.delimiter.c_str(), options.delimiter.c_str());
+		}
+		else
+		{
+			fprintf(fp, "Name%sTime\n", options.delimiter.c_str());
+		}
 
-        fprintf(fp,
-                "=============================================================================\n");
+		fprintf(fp, "=============================================================================\n");
 
-        std::map<std::string, long> cummulative_time = get_cummulative_times();
+		std::map<std::string, long> cummulative_time = get_cummulative_times();
 
-        for (auto pair : cummulative_time)
-        {
-            std::string str_times = dlog_to_string(pair.second);
+		for (auto pair : cummulative_time)
+		{
+			std::string str_times = dlog_to_string(pair.second);
 
-            if (options.human_readable == true)
-            {
-                std::string hr = "  (" + humanreadabletime(pair.second)
-                        + " )";
-                fprintf(fp, "%s%s%s%s%s\n", pair.first.c_str(),
-                        options.delimiter.c_str(), str_times.c_str(),
-                        options.delimiter.c_str(), hr.c_str());
-            }
-            else
-            {
-                fprintf(fp, "%s%s%s\n", (pair.first).c_str(),
-                        options.delimiter.c_str(), str_times.c_str());
-            }
-        }
-    }
+			if (options.human_readable == true)
+			{
+				std::string hr = "  (" + humanreadabletime(pair.second) + " )";
+				fprintf(fp, "%s%s%s%s%s\n", pair.first.c_str(), options.delimiter.c_str(), str_times.c_str(), options.delimiter.c_str(), hr.c_str());
+			}
+			else
+			{
+				fprintf(fp, "%s%s%s\n", (pair.first).c_str(), options.delimiter.c_str(), str_times.c_str());
+			}
+		}
+	}
 
-    void csv_events(FILE *fp) {
-        fprintf(fp, "event,begin,end\n");
+	void csv_events(FILE *fp)
+	{
+		fprintf(fp, "event,begin,end\n");
 
-        std::map<std::string, int> occurrences;
+		std::map<std::string, int> occurrences;
 
-        for (event_t &event : events)
-            occurrences[event.name] = 0;
+		for (event_t &event : events)
+			occurrences[event.name] = 0;
 
-        for (event_t &event : events) {
-            fprintf(fp, "%s[%d],\"%ld\",\"%ld\"\n", event.name, occurrences[event.name], time_to_long(event.begin), time_to_long(event.end));
-            occurrences[event.name] += 1;
-        }
-        fprintf(fp, ",,\n");
-    }
+		for (event_t &event : events)
+		{
+			fprintf(fp, "%s[%d],\"%ld\",\"%ld\"\n", event.name, occurrences[event.name], time_to_long(event.begin), time_to_long(event.end));
+			occurrences[event.name] += 1;
+		}
+		fprintf(fp, ",,\n");
+	}
+
+	void events_html_gantt(std::string filename, std::string tablename = "unnamed", std::string path = "")
+	{
+
+		if (path == "")
+			path = getenv("DLOG_OUTPUT_FOLDER");
+
+		DLOG_TABLE table(filename.c_str(), tablename.c_str(), path);
+		std::vector < std::string > head_row;
+
+		head_row.push_back("Event");
+		head_row.push_back("Begin");
+		head_row.push_back("End");
+
+		std::map<std::string, int> occurrences;
+
+		for (event_t &event : events)
+			occurrences[event.name] = 0;
+
+		long min = std::numeric_limits<long>::max();
+		for (event_t &event : events)
+		{
+			long converted= time_to_long(event.begin);
+			min = (min< converted)?min:converted;
+		}
+
+		std::cout<<"Min "<<min<<"\n";
+
+		for (event_t &event : events)
+		{
+			std::vector < std::string > row;
+			row.push_back(event.name);
+			row.push_back(std::to_string( time_to_long(event.begin) - min));
+			row.push_back(std::to_string(time_to_long( event.end) - min));
+			table.insert_row(row);
+			occurrences[event.name] += 1;
+		}
+		table.table_gantt_dump();
+	}
+
 };
 
 #endif /* DLOG_TIMER_HPP_ */
