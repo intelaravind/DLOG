8ca5666ab93a97d29442af2fcd27195f2b45075f
Aravind Sukumaran Rajam <aravind.sukumaran-rajam@inria.fr>
2015-04-10 18:30:20 +0200

    modifying recover.sh for autoconf patch

---
+++ recover.template.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Author Aravind
+
+SRC_PATH="autoconf will replace me"
+
+search_dir=`pwd`
+#echo $search_dir
+for entry in "$search_dir"/*.tag
+do
+#  echo "${entry%.*}"
+$SRC_PATH/DLOG_FINALIZER.out ${entry%.*}
+done
+
+
+
+
